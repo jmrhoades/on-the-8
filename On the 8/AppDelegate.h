f3e9522975deb68cff77a510ce06f99999460cb7
justinrhoades <justin.rhoades@gmail.com>
2014-09-19 12:54:42 -0700

    Initial Commit

---
+++ On the 8/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  On the 8
+//
+//  Created by Justin Rhoades on 9/19/14.
+//  Copyright (c) 2014 Justin Rhoades. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ On the 8/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  On the 8
+//
+//  Created by Justin Rhoades on 9/19/14.
+//  Copyright (c) 2014 Justin Rhoades. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
